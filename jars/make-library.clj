e84dcca74ba080672b888c0be6938e86a2be8772
Konrad Hinsen <hinsen@cnrs-orleans.fr>
2011-04-05 17:13:40 +0200

    Use current directory also when run from cake

--- jars/make-library.clj
+++ jars/make-library.clj
@@ -1,6 +1,9 @@
 (ns e-paper.make-library
   (:require [e-paper.storage :as ep]))
 
+(def cwd (try @(ns-resolve 'cake '*pwd*)
+              (catch Exception e (System/getProperty "user.dir"))))
+
 (defn parse-jar-spec
   [jar-spec]
   (let [[_ ds-name jar-file]
@@ -12,7 +15,7 @@
 
 (let [[library-file & jar-specs] *command-line-args*
       jar-specs (map parse-jar-spec jar-specs)
-      library (ep/create library-file)]
+      library (ep/create (str cwd "/" library-file))]
   (doseq [[ds-name jar-file] jar-specs]
-    (ep/store-jar library ds-name jar-file))
+    (ep/store-jar library ds-name (str cwd "/" jar-file)))
   (ep/close library))
