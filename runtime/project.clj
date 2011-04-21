8c9bf96afb5b0f4acb0fa0c24ed3f63f24521b25
Konrad Hinsen <hinsen@cnrs-orleans.fr>
2011-04-21 16:28:16 +0200

    AOT-compile runtime

--- runtime/project.clj
+++ runtime/project.clj
@@ -1,7 +1,7 @@
 (defproject e-paper-runtime "0.0.1-SNAPSHOT"
   :description "Runtime library for Executable Paper Grand Challenge submission"
   :author      "Konrad Hinsen <research@khinsen.fastmail.net>"
-  ;:aot [e-paper-runtime.data]
+  :aot [e-paper-runtime.data]
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
