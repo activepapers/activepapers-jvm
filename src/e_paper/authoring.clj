919b7d9b90bb8a5fe6591389fca2d50236bb816e
Konrad Hinsen <hinsen@cnrs-orleans.fr>
2011-05-02 18:17:58 +0200

    Build clojure scripts using the run-time executing mechanism

--- src/e_paper/authoring.clj
+++ src/e_paper/authoring.clj
@@ -5,46 +5,16 @@
   (:import e_paper.ExecutablePaperRef)
   (:require [clojure.contrib.pprint :as pprint]))
 
-(defn prepare-script
-  ([paper]
-   (prepare-script paper nil))
-  ([paper name]
-   (security/start-secure-mode)
-   (let [cl (.getClassLoader ExecutablePaperRef)
-         acc (:accessor paper)]
-     (ExecutablePaperRef/setAccessors cl
-       acc
-       (if (isa? (class acc)
-                 ch.systemsx.cisd.hdf5.IHDF5Writer)
-         acc
-         nil))
-     (ExecutablePaperRef/setCurrentCalclet cl (str "/code/" name))
-     (ExecutablePaperRef/initializeDependencyList cl))
-   (when (not (nil? name))
-     (remove-ns name))))
-
-(defn cleanup-script
-  []
-  (let [cl (.getClassLoader ExecutablePaperRef)]
-    (ExecutablePaperRef/clearDependencyList cl)
-    (ExecutablePaperRef/setCurrentCalclet cl nil)
-    (ExecutablePaperRef/setAccessors cl nil nil)))
-
 (defmacro clojure-script
   [paper jars & body]
   (assert (= (first (first body)) 'ns))
   (let [name (second (first body))]
     `(do
-       (storage/store-program ~paper (str '~name) ~jars "clojure.launcher"
-             [{:type :text-file :contents (apply str (map prn-str '~body))}])
-       (let [~'current-ns (ns-name *ns*)]
-         (prepare-script ~paper '~name)
-         (try
-           (doseq [~'form '~body]
-             (eval ~'form))
-           (finally
-            (cleanup-script)
-            (in-ns ~'current-ns)))))))
+       (let [ds# (storage/store-program
+                   ~paper (str '~name) ~jars "clojure.launcher"
+                   [{:type :text-file
+                     :contents (apply str (map prn-str '~body))}])]
+         (execution/run-calclet ds#)))))
 
 (defn script
   [paper name script-engine jars text]
