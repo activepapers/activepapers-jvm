30d47f438762ebe2ab840fe2917f50a9c044575c
Konrad Hinsen <hinsen@cnrs-orleans.fr>
2011-05-12 19:02:00 +0200

    New runtime function read-data

--- clojure-runtime/src/e_paper_runtime/data.clj
+++ clojure-runtime/src/e_paper_runtime/data.clj
@@ -13,19 +13,12 @@
   (let [node (DataAccess/getData name)]
     (hdf5/make-hdf-node (.getReader node) (.getPath node))))
 
-(comment
-  (defn get-data
-    [name]
-    (let [reader     (ExecutablePaperRef/getReader)
-          root       (hdf5/make-hdf-node reader "/")
-          data-group (hdf5/lookup root "data")]
-      (when (nil? reader)
-        (throw (Exception. "no e-paper open")))
-      (let [ds (hdf5/lookup data-group name)]
-        (ExecutablePaperRef/addDependency (:path ds))
-        (dereference ds)))))
+(defn read-data
+  [name]
+  (let [node (DataAccess/getData name)]
+    (hdf5/read (hdf5/make-hdf-node (.getReader node) (.getPath node)))))
 
-;
+                                        ;
 ; Creation of a new dataset, with all the required attributes.
 ;
 (defn create-data
@@ -37,36 +30,3 @@
     (DataAccess/finalizeData node)
     ds))
 
-(comment
-  (defn create-data
-    [name data]
-    (let [writer  (ExecutablePaperRef/getWriter)
-          calclet (ExecutablePaperRef/getCurrentCalclet)]
-      (when (nil? writer)
-        (throw (Exception. 
-                (if (nil? (ExecutablePaperRef/getReader))
-                  "no e-paper open"
-                  "e-paper not writable"))))
-      (when (nil? calclet)
-        (throw (Exception. "no calclet active")))
-      (let [root       (hdf5/make-hdf-node writer "/")
-            data-group (hdf5/lookup root "data")]
-        (when-let [ds (hdf5/lookup data-group name)]
-          (let [creator (hdf5/get-attribute ds "e-paper-generating-calclet")]
-            (if (or (nil? creator) (not= (hdf5/read creator) calclet))
-              (throw (Exception. (str "Overwriting dataset " name " created by "
-                                      "calclet " (hdf5/read creator)))))))
-        (let [ds   (hdf5/create-dataset data-group name data)
-                                        ; all datasets read by the calclet so far are dependencies
-              deps (vec (.toArray (ExecutablePaperRef/getDependencyList)
-                                  (make-array String 0)))
-                                        ; add the calclet itself as a dependency
-              deps (conj deps calclet)
-                                        ; and also add the calclet's jar files
-              deps (concat deps (-> (hdf5/lookup root (subs calclet 1))
-                                    (hdf5/get-attribute "jvm-jar-files")
-                                    (hdf5/read)))]
-          (hdf5/create-attribute ds "e-paper-datatype" "data")
-          (hdf5/create-attribute ds "e-paper-generating-calclet" calclet)
-          (hdf5/create-attribute ds "e-paper-dependencies" deps)
-          ds)))))
