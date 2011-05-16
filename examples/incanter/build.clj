7778443777ec82e19eb7ce4bdcd8161bd8e94ffd
Konrad Hinsen <hinsen@cnrs-orleans.fr>
2011-05-16 13:12:42 +0200

    Updated examples

--- examples/incanter/build.clj
+++ examples/incanter/build.clj
@@ -1,18 +1,26 @@
-(ns build-paper
+(ns build-incanter-example
   (:require [e-paper.storage :as ep])
   (:use [e-paper.authoring :only (clojure-script)])
-  (:use [e-paper.execution :only (run-calclet)])
-  (:import java.io.File))
+  (:use [e-paper.execution :only (run-calclet)]))
 
-(def paper (ep/create (File. "incanter_example.h5")))
+; Create an empty e-paper
+(def paper (ep/create (java.io.File. "incanter_example.h5")))
 
+; Add references to the jar files from the Incanter library
 (def jars (ep/store-library-references paper "incanter"))
 
-; Input parameters
+; Store input parameters for calclets
 (ep/create-data paper "frequency" 0.2)
 (ep/create-data paper "time" (vec (range 0. 10. 0.1)))
 
-; Computations are run immediately.
+; Define and run a computational calclet. The clojure-script function
+; knows how to run Clojure scripts and takes the name of the calclet
+; (calc-sine) from the Clojure namespace declaration. The jars define
+; the Java classpath for the execution of the script.
+
+; The runtime library records that this calclet reads "time" and
+; "frequency" and creates "sine".  This information is used to create
+; the dependency graph.
 (run-calclet
  (clojure-script paper jars
    (ns calc-sine
@@ -22,7 +30,8 @@
          sine      (map #(Math/sin (* 2 Math/PI frequency %)) time)]
      (create-data "sine" sine))))
 
-; Visualization
+; Define (but do not run) a visualization calclet. It creates a simple
+; line plot of "time" vs. "sine".
 (clojure-script paper jars
   (ns view-sine
     (:use [e-paper-runtime.data])
@@ -31,7 +40,8 @@
   (view (xy-plot (read-data "time") (read-data "sine")
                  :x-label "time" :y-label "sine")))
 
-; Table
+; Define (but do not run) a visualization calclet. It a tabular
+; representation of "time" vs. "sine".
 (clojure-script paper jars
   (ns sine-table
     (:use [e-paper-runtime.data])
@@ -40,7 +50,9 @@
   (view (col-names (conj-cols (read-data "time") (read-data "sine"))
                    ["time" "sine"])))
 
-; REPL is for calclet development
+; Define a calclet that runs a Clojure read-eval-print loop (REPL) in
+; the environment of this e-paper. The REPL allows interactive data
+; analysis and line-by-line development of calclets.
 (clojure-script paper jars
   (ns repl
     (:require clojure.main)
@@ -51,4 +63,5 @@
     (:use [incanter.stats]))
   (clojure.main/repl))
 
+; Close the e-paper file.
 (ep/close paper)
--- examples/python/build.clj
+++ examples/python/build.clj
@@ -1,7 +1,7 @@
 (ns build-python-example
   (:require [e-paper.storage :as ep])
-  (:require [e-paper.execution :as execution])
-  (:require [e-paper.authoring :as auth]))
+  (:use [e-paper.authoring :only (script)])
+  (:use [e-paper.execution :only (run-calclet)]))
 
 ; Create an empty e-paper
 (def paper (ep/create (java.io.File. "python_example.h5")))
@@ -17,12 +17,13 @@
 ; Define and run a computational calclet. The name of the calclet
 ; is "calc-sine" and it is defined as a script using the scripting
 ; engine called "python" which is part of the Jython standard library.
+; The jars define the Java classpath for the execution of the script.
 
 ; The runtime library records that this calclet reads "time" and
 ; "frequency" and creates "sine".  This information is used to create
 ; the dependency graph.
-(execution/run-calclet
- (auth/script paper "calc-sine" "python" jars
+(run-calclet
+ (script paper "calc-sine" "python" jars
 "
 from e_paper_data import readData, createData
 import math
@@ -37,7 +38,7 @@ createData(\"sine\", sine)
 
 ; Define (but do not run) a visualization calclet. It creates a simple
 ; line plot of "time" vs. "sine".
-(auth/script paper "view-sine" "python" (vec (concat jars plotting-jars))
+(script paper "view-sine" "python" (vec (concat jars plotting-jars))
 "
 from e_paper_data import readData, createData
 from e_paper_plotting import plot
