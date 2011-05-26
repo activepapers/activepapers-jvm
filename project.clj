8851a5dbb09dcad9e763f4edc20f7a116098cd93
Konrad Hinsen <hinsen@cnrs-orleans.fr>
2011-05-26 14:33:42 +0200

    Remove project.clj which is long outdated

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject e-paper "0.0.1-SNAPSHOT"
-  :description "Executable Paper Grand Challenge submission"
-  :author      "Konrad Hinsen <research@khinsen.fastmail.net>"
-  :dependencies [[clojure "1.2.1"]
-                 [clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
