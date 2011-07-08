9dc5fb814adc5b4520ad22860c9e62b4d62992ec
Konrad Hinsen <hinsen@cnrs-orleans.fr>
2011-07-08 16:28:50 +0200

    Updated Clojure scripting example (but it still doesn't work)

--- examples/clojure_scripting_example/build.clj
+++ examples/clojure_scripting_example/build.clj
@@ -1,32 +1,26 @@
-(ns build-clojure-script
-  (:require [e-paper.storage :as ep])
-  (:use [e-paper.authoring :only (script)])
-  (:use [e-paper.execution :only (run-calclet)])
+(ns clojure-scripting.example
+  (:require [active-papers.storage :as ep])
+  (:use [active-papers.authoring :only (script)])
+  (:use [active-papers.execution :only (run-calclet)])
   (:import java.io.File))
 
-(def dir (File. "/Users/hinsen/projects/e-paper/examples/clojure-script/"))
-
-(def paper (ep/create (File. dir "clojure_paper.h5")))
+(def paper (ep/create (File. "clojure_example.h5")))
 
 (def jars (ep/store-library-references paper "clojure"))
-
-(prn "!")
 
 ; The real calclets are run immediately.
 (run-calclet
  (script paper "generate-input" "Clojure" jars
 " (ns generate-input
-    (:require [e-paper-runtime.data :as data]))
+    (:require [active-paper-clojure-runtime.data :as data]))
   (data/create-data \"time\" (vec (range 0. 10. 0.1)))
   (data/create-data \"frequency\" 0.2)
 "))
 
-(prn "!")
-
 (run-calclet
  (script paper "calc-sine" "Clojure" jars
 " (ns calc-sine
-    (:require [e-paper-runtime.data :as data])
+    (:require [active-paper-clojure-runtime.data :as data])
     (:require [clj-hdf5.core :as hdf5]))
   (let [time      (hdf5/read (data/get-data \"time\"))
         frequency (hdf5/read (data/get-data \"frequency\"))
@@ -34,27 +28,21 @@
     (data/create-data \"sine\" sine))
 "))
 
-(prn "!")
-
 ; REPL and swank-server are for calclet development
 (script paper "repl" "Clojure" jars
 "(ns repl
     (:require clojure.main)
-    (:require [e-paper-runtime.data :as data])
+    (:require [active-paper-clojure-runtime.data :as data])
     (:require [clj-hdf5.core :as hdf5]))
  (clojure.main/repl)
 ")
 
-(prn "!")
-
 (script paper "swank-server" "Clojure" jars
 "(ns calclet
-    (:require [e-paper-runtime.data :as data])
+    (:require [active-paper-clojure-runtime.data :as data])
     (:require [clj-hdf5.core :as hdf5])
     (:require [swank.swank]))
  (swank.swank/start-repl)
 ")
-
-(prn "!")
 
 (ep/close paper)
